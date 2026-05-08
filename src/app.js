667aca52584fe156d812cc9c11dbe9b2447fa7fc
Natalia Stiopu <stiopuaurora@gmail.com>
2026-05-08 10:09:28 +0000

    generador de escusas final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function randomExcuse(anArray) {
+  const index = Math.floor(Math.random() * anArray.length);
+  return anArray[index];
+}
+
+function generateExcuse() {
+  const excuse = `${randomExcuse(who)} ${randomExcuse(action)} ${randomExcuse(what)} ${randomExcuse(when)}.`;
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const excuse = document.getElementById ('excuse');
+  excuse.innerText = generateExcuse();
+}
+
