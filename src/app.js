e8edc00424071090ebabf7a88ee1e276f57f50c9
Natalia Stiopu <stiopuaurora@gmail.com>
2026-05-13 10:24:45 +0000

    Revision y correcciones

--- src/app.js
+++ src/app.js
@@ -5,23 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function randomExcuse(anArray) {
-  const index = Math.floor(Math.random() * anArray.length);
+window.onload = function() {
+
+function getRandomElement(anArray) {
+  let index = Math.floor(Math.random() * anArray.length);
   return anArray[index];
 }
 
 function generateExcuse() {
-  const excuse = `${randomExcuse(who)} ${randomExcuse(action)} ${randomExcuse(what)} ${randomExcuse(when)}.`;
-  return excuse;
+  let randomExcuse = `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+  return randomExcuse;
 }
 
-window.onload = function() {
-  const excuse = document.getElementById ('excuse');
-  excuse.innerText = generateExcuse();
+  let randomExcuse = document.getElementById ('excuse');
+  randomExcuse.innerText = generateExcuse();
 }
 
